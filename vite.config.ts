a9c60b48dc0f3bb3080763738da1d3762f3497db
Gabriel Souza <gsouzaa4@gmail.com>
2024-09-27 00:45:20 +0100

    Configured for deployment to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/fitness-website/',
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        404: './404.html',
+      },
+    },
+  },
+});
