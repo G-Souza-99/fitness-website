7ea3eda2c8ce14ae30aa0a778f31e06aa9cf227d
Gabriel Souza <gsouzaa4@gmail.com>
2024-09-27 09:30:30 +0100

    setup 404

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: './index.html',
-        404: './404.html',
+        main: 'index.html',
+        404: './public/404.html',
       },
     },
   },
